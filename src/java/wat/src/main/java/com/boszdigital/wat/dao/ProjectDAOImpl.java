04656eab629305b472c030af55d1a6659c723deb
Diego Wong <diego.wong@boszdigital.com>
2013-04-03 16:26:50 -0500

    Fix application context and add application.properties file

--- src/java/wat/src/main/java/com/boszdigital/wat/dao/ProjectDAOImpl.java
+++ src/java/wat/src/main/java/com/boszdigital/wat/dao/ProjectDAOImpl.java
@@ -1,8 +1,11 @@
 package com.boszdigital.wat.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.boszdigital.wat.model.Project;
 
 
+@Repository
 public class ProjectDAOImpl extends GenericDAOImpl<Project, Long> implements ProjectDAO
 {
 
